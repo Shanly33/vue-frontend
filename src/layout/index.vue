dcdd14d34414df229425f4319d51fb733663aaaf
Shanly33 <shanlyze@163.com>
2024-09-13 00:45:08 +0800

    静态菜单展示

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,6 +3,19 @@
     <!-- 左侧菜单 -->
     <div class="layout_slider">
       <Logo></Logo>
+      <!-- 菜单 -->
+      <el-scrollbar class="scrollbar">
+        <el-menu class="custom-menu">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <el-sub-menu index="3">
+            <template #title>权限管理</template>
+            <el-menu-item index="2-1">用户管理</el-menu-item>
+            <el-menu-item index="2-2">角色管理</el-menu-item>
+            <el-menu-item index="2-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航 -->
     <div class="layout_nav">456</div>
@@ -24,6 +37,13 @@ import Logo from './logo/index.vue'
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+      .custom-menu {
+        --el-menu-bg-color: #001e3a;
+      }
+    }
   }
   .layout_nav {
     position: fixed;
